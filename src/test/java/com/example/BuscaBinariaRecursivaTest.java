d637fb5872b5ec6c3c080d44d953291374045380
Lucas <lucas.defensor.90@gmail.com>
2025-03-20 23:35:57 -0300

    Update casos testes

--- src/test/java/com/example/BuscaBinariaRecursivaTest.java
+++ src/test/java/com/example/BuscaBinariaRecursivaTest.java
@@ -6,16 +6,37 @@ import static org.junit.Assert.*;
 public class BuscaBinariaRecursivaTest {
 
     @Test
-    public void testBuscaBinaria() {
+    public void testBuscaBinaria_ElementoNoMeio() {
         int[] array = {1, 3, 5, 7, 9, 11, 13, 15};
         int resultado = BuscaBinariaRecursiva.buscaBinaria(array, 7, 0, array.length - 1);
         assertEquals(3, resultado); // Verifica se o índice do elemento 7 é 3
     }
 
     @Test
-    public void testElementoNaoEncontrado() {
+    public void testBuscaBinaria_ElementoNoInicio() {
+        int[] array = {1, 3, 5, 7, 9, 11, 13, 15};
+        int resultado = BuscaBinariaRecursiva.buscaBinaria(array, 1, 0, array.length - 1);
+        assertEquals(0, resultado); // Verifica se o índice do elemento 1 é 0
+    }
+
+    @Test
+    public void testBuscaBinaria_ElementoNoFim() {
+        int[] array = {1, 3, 5, 7, 9, 11, 13, 15};
+        int resultado = BuscaBinariaRecursiva.buscaBinaria(array, 15, 0, array.length - 1);
+        assertEquals(7, resultado); // Verifica se o índice do elemento 15 é 7
+    }
+
+    @Test
+    public void testBuscaBinaria_ElementoNaoEncontrado() {
         int[] array = {1, 3, 5, 7, 9, 11, 13, 15};
         int resultado = BuscaBinariaRecursiva.buscaBinaria(array, 10, 0, array.length - 1);
         assertEquals(-1, resultado); // Verifica se o elemento 10 não foi encontrado
+    }
+
+    @Test
+    public void testBuscaBinaria_ArrayVazio() {
+        int[] array = {};
+        int resultado = BuscaBinariaRecursiva.buscaBinaria(array, 7, 0, array.length - 1);
+        assertEquals(-1, resultado); // Verifica se a busca em um array vazio retorna -1
     }
 }
